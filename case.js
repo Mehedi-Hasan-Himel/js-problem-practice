bf4ad565fe16823b1a13cc5fdedbb94f15edc323
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-02-03 23:39:11 +0600

    [ADD] case

---
+++ case.js
@@ -0,0 +1,9 @@
+const userName = `blackPink`
+const userInput = `blackPinK`
+
+if (userName.toLocaleLowerCase() === userInput.toLocaleLowerCase()) {
+   console.log(`Valid User`);
+
+} else {
+   console.log(`Invalid User`);
+}
