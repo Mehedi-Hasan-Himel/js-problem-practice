6104e0a5bc68d2662383072eb77de1426173dc61
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-02-05 10:13:46 +0600

    [ADD] tallest

--- tallest.js
+++ tallest.js
@@ -2,8 +2,7 @@ function maxInArray(numbers) {
   let largest = numbers[0];
 
   for (let i = 0; i <= numbers.length; i++) {
-    const index = i;
-    const element = numbers[index];
+    const element = numbers[i];
     console.log(element);
     if (element > largest) {
       largest = element;
@@ -12,10 +11,10 @@ function maxInArray(numbers) {
   return largest;
 }
 
-const heights = [167, 199, 120, 165, 137];
+const heights = [167, 199, 1200, 165, 137, 3];
 const tallest = maxInArray(heights);
 
 console.log(`Tallest person is : `, tallest);
 
+// HW: Write a function to get the lowest number in an array.
 
-// HW: Write a function to get the lowest number in an array. 
