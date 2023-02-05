9803025a6f1cc3f9d8e6a0cc56aac7fef8a6667b
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-02-05 10:19:13 +0600

    [ADD] smallest

--- tallest.js
+++ tallest.js
@@ -1,7 +1,7 @@
 function maxInArray(numbers) {
   let largest = numbers[0];
 
-  for (let i = 0; i <= numbers.length; i++) {
+  for (let i = 0; i < numbers.length; i++) {
     const element = numbers[i];
     console.log(element);
     if (element > largest) {
@@ -18,3 +18,20 @@ console.log(`Tallest person is : `, tallest);
 
 // HW: Write a function to get the lowest number in an array.
 
+function minInArray(numbers) {
+  let smallest = numbers[0];
+  for (let i = 0; i < numbers.length; i++) {
+    const element = numbers[i];
+    console.log(element);
+    if (element < smallest) {
+      smallest = element;
+    }
+  }
+  return smallest;
+}
+
+const heights2 = [167, 199, 1200, 165, 137, 3];
+
+const smallest = minInArray(heights2);
+
+console.log(`Smallest person is : `, smallest);
