ec649d7ec5b79d733a8dd93084e1ce4951e80471
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-02-05 08:39:34 +0600

    [ADD] practice code

--- search.js
+++ search.js
@@ -1,9 +1,27 @@
 const lyrics = `Tumi Bondhu Kala Pakhi ami jeno ki , bosonto kale tomay bolte pari ni.`;
 
-// const doesExist = lyrics.includes(`Pakhi`)
-const lyricsLowerCase = lyrics.toLowerCase();
+const searchString = `pakhi`;
 
-const searchStringLower = searchStringLower.toLowerCase();
+const lyricsLowerCase = lyrics.toLowerCase();
+const searchStringLower = searchString.toLowerCase();
 const doesExist = lyricsLowerCase.includes(searchStringLower);
 
 console.log(doesExist);
+/*--------------------------------------------------------------------*/
+
+// index of
+console.log(lyrics.indexOf(`Kala`));
+
+if (lyrics.indexOf(`sada`) !== -1) {
+  console.log(`Sada Ache`);
+} else {
+  console.log(`Sada nai`);
+}
+
+// startwith
+console.log(lyrics.startsWith(`2mi`));
+
+// endsWith
+const fileName = `mybiodata.pdf`;
+const otherfile = `mypic.png`;
+fileName.endsWith(`.pdf`);
