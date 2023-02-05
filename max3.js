0a3c0d061d815fd846e2e0af62b2bfdfc97de915
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-02-05 09:34:05 +0600

    [ADD] practice code

---
+++ max3.js
@@ -0,0 +1,25 @@
+const jim = 69;
+const dela = 97;
+const chinku = 99;
+
+function getMax(params) {}
+
+console.log(Math.max(jim, dela, chinku));
+// if (jim > dela && jim > chinku) {
+//   console.log(`Jim will get the cake`);
+// } else if (dela > jim && dela > chinku) {
+//   console.log(`Dele will get the cake`);
+// } else {
+//   console.log(`Chinku will get the cake`);
+// }
+
+// HW-01:
+// Write a function that will take 3 numbers will return the max number
+
+// HW-02:
+// Write a functio that will take 3 parameter and will return the min number
+
+
+// condition: 
+// first time do it using if-else
+// second time do it using Math.min or Math.max
